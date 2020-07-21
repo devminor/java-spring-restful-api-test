49bd803f03d93f99af5429effbb23087eef8ec5f
kemal <hilmi.alyaz@freelancer.run>
2020-07-21 15:51:58 +0100

    UnsupportedOperationException fixed arraylist

--- src/main/java/uk/co/huntersix/spring/rest/referencedata/PersonDataService.java
+++ src/main/java/uk/co/huntersix/spring/rest/referencedata/PersonDataService.java
@@ -1,25 +1,27 @@
 package uk.co.huntersix.spring.rest.referencedata;
 
 import org.springframework.stereotype.Service;
+import uk.co.huntersix.spring.rest.exception.EntityAlreadyExistException;
 import uk.co.huntersix.spring.rest.exception.EntityNotFoundException;
 import uk.co.huntersix.spring.rest.model.Person;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class PersonDataService {
-    public static final List<Person> PERSON_DATA = Arrays.asList(
-        new Person("Mary", "Smith"),
-        new Person("Brian", "Archer"),
-        new Person("Collin", "Brown")
-    );
+    public static final List<Person> PERSON_DATA = new ArrayList<>(Arrays.asList(
+            new Person("Mary", "Smith"),
+            new Person("Brian", "Archer"),
+            new Person("Collin", "Brown")
+    ));
 
     public Person findPerson(String lastName, String firstName) {
         return PERSON_DATA.stream()
-            .filter(p -> p.getFirstName().equalsIgnoreCase(firstName)
-                    && p.getLastName().equalsIgnoreCase(lastName))
+                .filter(p -> p.getFirstName().equalsIgnoreCase(firstName)
+                        && p.getLastName().equalsIgnoreCase(lastName))
                 .findFirst().orElseThrow(EntityNotFoundException::new)
                 ;
     }
@@ -30,7 +32,15 @@ public class PersonDataService {
                 .collect(Collectors.toList());
     }
 
-    public Person addPerson(Person person) {
-        return null;
+    public Person addPerson(Person p) {
+        PERSON_DATA.stream()
+                .filter(x -> x.getFirstName().equalsIgnoreCase(p.getFirstName())
+                        && x.getLastName().equalsIgnoreCase(p.getLastName()))
+                .findAny().ifPresent(s -> {
+            throw new EntityAlreadyExistException();
+        });
+
+        PERSON_DATA.add(p);
+        return p;
     }
 }
