a1c1407bc3285228f5bd0b51c97daf8a225dbf6c
kemal <hilmi.alyaz@freelancer.run>
2020-07-21 09:19:36 +0100

    http request test entity not found exception .

--- src/test/java/uk/co/huntersix/spring/rest/controller/HttpRequestTest.java
+++ src/test/java/uk/co/huntersix/spring/rest/controller/HttpRequestTest.java
@@ -7,10 +7,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.co.huntersix.spring.rest.referencedata.PersonDataService;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -29,5 +38,13 @@ public class HttpRequestTest {
                 String.class
             )
         ).contains("Mary");
+    }
+
+    @Test
+    public void shouldReturnNotFoundWhenPersonNotFound() {
+        Map entity = this.restTemplate.getForObject("http://localhost:" + port + "/person/kmd/trn", Map.class);
+        assertNotNull(entity);
+        assertEquals(404, entity.get("status"));
+        assertEquals("Entity not found", entity.get("message"));
     }
 }
